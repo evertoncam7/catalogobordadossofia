4bc331eb6289b89ef1daf03be3b6b391898c1cfc
evertoncam7 <everton.camargo.cam7@gmail.com>
2020-07-16 22:21:22 -0300

    removendo um produto dupliacado

--- assets/js/bd.js
+++ assets/js/bd.js
@@ -8,11 +8,10 @@ function createProduct(id, nomeProd, price, cat, slogan, desc, url){
 
 
 // CASAL
-createProduct(1, "Edredom - Giovana", 38, "Edredom", "Casal","2,90 x 2,90","assets/imagens/r/IMG-20200616-WA0005.jpg");
-createProduct(2, "Edredom - Juliana", 38, "Edredom", "Casal","2,90 x 2,90","assets/imagens/r/IMG-20200616-WA0008.jpg");
+createProduct(1, "Edredom - Juliana", 38, "Edredom", "Casal","2,90 x 2,90","assets/imagens/r/IMG-20200616-WA0005.jpg");
+createProduct(2, "Edredom - Giovana", 38, "Edredom", "Casal","2,90 x 2,90","assets/imagens/r/IMG-20200616-WA0008.jpg");
 createProduct(2, "Edredom - Melissa", 38, "Edredom", "Casal","2,90 x 2,90","assets/imagens/r/IMG-20200616-WA0010.jpg");
 createProduct(2, "Edredom - Gabriela", 38, "Edredom", "Casal","2,90 x 2,90","assets/imagens/r/IMG-20200616-WA0012.jpg");
-createProduct(2, "Edredom - Rebeca", 38, "Edredom", "Casal","2,90 x 2,90","assets/imagens/r/IMG-20200616-WA0010.jpg");
 
 // SOLTEIRO
 createProduct(2, "Edredom - Rebeca", 38, "Edredom", "Solteiro","2,90 x 2,90","assets/imagens/solteiro/solteiro1.jpeg");
