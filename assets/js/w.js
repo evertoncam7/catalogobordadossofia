72dd084aa48e64d3a2acd8983273ae2402d87f11
evertoncam7 <everton.camargo.cam7@gmail.com>
2021-03-11 21:07:09 -0300

    criando arquivo do whatsapp

---
+++ assets/js/w.js
@@ -0,0 +1,41 @@
+
+
+function currencyBrl(valor){
+
+    if (!valor) {
+        valor = 0;
+    }
+
+
+    const options = {style:"currency", currency:"BRL"};
+    return valor.toLocaleString("pt-br", options);
+
+}
+
+
+let string = "";
+const btn_whatsapp = document.getElementById("btn-w");
+btn_whatsapp.addEventListener("click", function(){
+
+    const product = [];
+    const qunatidade = [];
+    const preco = [];
+
+    
+
+    const data = JSON.parse(sessionStorage.cart);
+    data.produtos.map(function(values, indice){
+        string += values.qt+"%20"+values.product+"%20"+currencyBrl(values.preco)+",%20"
+        // console.log(string);
+    });
+    
+    window.location.href = `https://api.whatsapp.com/send?phone=5516982011978&text=${string}`;
+
+    console.log(string);
+});
+
+
+
+
+
+
