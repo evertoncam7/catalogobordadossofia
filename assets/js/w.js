c6569cbac5fa7bfb8e849ded3cf7278be9dffedc
evertoncam7 <everton.camargo.cam7@gmail.com>
2021-03-11 21:32:17 -0300

    w

--- assets/js/w.js
+++ assets/js/w.js
@@ -25,11 +25,11 @@ btn_whatsapp.addEventListener("click", function(){
 
     const data = JSON.parse(sessionStorage.cart);
     data.produtos.map(function(values, indice){
-        string += values.qt+"%20"+values.product+"%20"+currencyBrl(values.preco)+",%20"+values.img+"%20";
+        string += values.qt+"%20"+values.product+"%20"+currencyBrl(values.preco)+".%20";
         // console.log(string);
     });
     
-    window.location.href = `https://api.whatsapp.com/send?phone=5516982011978&text=${string}%20(Itens:%20${data.quantidade},%20Total do pedido:%20${currencyBrl(data.total)})`;
+    window.location.href = `https://api.whatsapp.com/send?phone=5516982011978&text=${string}%20(Quantidade%20de%20itens:%20${data.quantidade},%20Total%20do%20pedido:%20${currencyBrl(data.total)})`;
 
     console.log(string);
 });
